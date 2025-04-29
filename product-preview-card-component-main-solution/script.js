3c03feaa3b4c635989d0fdf61b42baeee873fc5b
Mohammad Humayu <mhumayu2017@gmail.com>
2025-04-29 15:49:11 +0530

    update index.html script.js p-p-c-c-m-s

--- product-preview-card-component-main-solution/script.js
+++ product-preview-card-component-main-solution/script.js
@@ -4,9 +4,9 @@ const img = document.querySelector('img');
 // Function to update image based on screen size
 function updateImage() {
   if (window.innerWidth >= 427) {
-    img.src = '/images/image-product-desktop.jpg'; // Tablet image
+    img.src = './images/image-product-desktop.jpg'; // Tablet image
   } else {
-    img.src = '/images/image-product-mobile.jpg'; // Mobile image
+    img.src = './images/image-product-mobile.jpg'; // Mobile image
   }
 }
 
